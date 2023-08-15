65d424aa43b00ca65cb7a1020cb839a95318d80e
Beatriz Salvador Parolin <beatrizparolin@ibm.com>
2023-08-15 09:55:43 -0300

    :tada: First commit

---
+++ src/data/words.js
@@ -0,0 +1,10 @@
+// Lista de palavras do jogo
+
+export const wordsList = {
+  país: ["Alemanha", "Rússia", "Angola", "Uganda", "Austrália"],
+  fruta: ["Banana", "Maçã", "Morango", "Melão", "Damasco"],
+  profissão: ["Veterinário", "Dentista", "Padeiro", "Escritor", "Tatuador"],
+  idioma: ["Francês", "Japonês", "Inglês", "Hindi", "Árabe"],
+  cor: ["Magenta", "Ciano", "Roxo", "Bege", "Caramelo"],
+  programação: ["JavaScript", "Cobol", "Java", "Python", "Dart"],
+};
